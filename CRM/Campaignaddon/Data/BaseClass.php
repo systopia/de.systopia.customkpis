c6b68208f7933b4931dccbd270030a21682777ad
Benedikt Magnus <systopia@benediktmagnus.de>
2019-03-19 15:43:48 +0100

    Base classes for data and KPIs.

---
+++ CRM/Campaignaddon/Data/BaseClass.php
@@ -0,0 +1,26 @@
+<?php
+/*-------------------------------------------------------+
+| SYSTOPIA OnlyOffice Integration                        |
+| Copyright (C) 2019 SYSTOPIA                            |
+| Author: B. Zschiedrich (zschiedrich@systopia.de)       |
++--------------------------------------------------------+
+| This program is released as free software under the    |
+| Affero GPL license. You can redistribute it and/or     |
+| modify it under the terms of this license which you    |
+| can read by viewing the included agpl.txt or online    |
+| at www.gnu.org/licenses/agpl.html. Removal of this     |
+| copyright header is strictly prohibited without        |
+| written permission from the original author(s).        |
++-------------------------------------------------------*/
+
+abstract class CRM_Campaignaddon_Data_BaseClass {
+
+  protected $name = 'BaseClass';
+
+  public function getName() {
+    return $this->name;
+  }
+
+  abstract public function getData();
+
+}
